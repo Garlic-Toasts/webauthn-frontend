84f5bfc237d7433fbd4259c56d759c93e0ce6339
Gregory-coder <gr.eydelkind@gmail.com>
2024-04-20 19:57:37 +0300

    day 1 final version

--- public/static/js/sign-up.js
+++ public/static/js/sign-up.js
@@ -1,2 +1,26 @@
-const { startRegistration } = SimpleWebAuthnBrowser;
-startRegistration();
+const { startRegistration,browserSupportsWebAuthnAutofill } = SimpleWebAuthnBrowser;
+
+async function signUp()
+{
+    const login = document.getElementById('login').value;
+    console.log(login)
+
+    const resp = await fetch('/generate-registration-options');
+
+    let attResp = await startRegistration(await resp.json());
+
+    const verificationResp = await fetch('/verify-registration', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(attResp),
+    });
+
+    const verificationJSON = await verificationResp.json();
+
+    console.log(verificationJSON)
+}
+
+//startRegistration();
+browserSupportsWebAuthnAutofill();
