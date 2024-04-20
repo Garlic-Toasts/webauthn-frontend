f26db45bbba4ecbca451ef09e1476429da3e73c2
Gregory-coder <gr.eydelkind@gmail.com>
2024-04-20 14:08:48 +0300

    added routing

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-const express = require("express");
+const express = require('express');
 const path = require('path');
 
 const app = express();
+app.use('/static', express.static(path.join(__dirname, '/public/static')));
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '/public/landing/index.html'));
+    res.sendFile(path.join(__dirname, '/public/views/landing/index.html'));
+});
+
+app.get('/sign-in', function(req, res) {
+    res.sendFile(path.join(__dirname, '/public/views/sign-in/index.html'));
+});
+
+app.get('/sign-up', function(req, res) {
+    res.sendFile(path.join(__dirname, '/public/views/sign-up/index.html'));
 });
 
 app.listen(3001);
